37a48127f40adf9c9bebf67e955018341852b6d3
Projetphp <alexandre.capaldi@hotmail.fr>
2021-03-15 15:08:29 +0100

    stack-method-ok

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -5,22 +5,52 @@ class StackDataStructure {
   }
 
   canPush() {
-    // ... your code goes here
+    // can i push some content ?
+    // If more than 10, no
+
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      return false
+    } else {
+      return true
+    }
+
+
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl;
+
   }
 
   isEmpty() {
-    // ... your code goes here
+
+
+    if (this.stackControl.length === 0) {
+      return true
+    } else {
+      return false
+    }
+
   }
 
   push(item) {
-    // ... your code goes here
+
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl;
+
+    } else {
+      return 'Stack Overflow'
+    }
+
   }
 
   pop() {
-    // ... your code goes here
+
+    if (this.isEmpty() === false) {
+      this.stackControl.pop()
+    } else {
+      return 'Stack Underflow'
+    }
   }
 }
